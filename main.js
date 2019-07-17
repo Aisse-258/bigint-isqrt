c6961acde2504c6f7ffb05a114fb9c3a3f819173
Aisse-258 <y-kate@ukr.net>
2019-07-17 13:42:30 +0300

    Fix align

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 var sqrt = function (value) {
-    if (value < 2n) {
-        return value;
-    }
+	if (value < 2n) {
+		return value;
+	}
 
 	if (value < 16n) {
 		return BigInt(Math.floor(Math.sqrt(Number(value))));
 	}
 	
-    if(value < (1n << 52n)){
-        var x1 = BigInt(Math.floor(Math.sqrt(Number(value))))-3n;
-    } else {
-        var x1 = (1n << 52n) - 2n;
-    }
+	if(value < (1n << 52n)){
+		var x1 = BigInt(Math.floor(Math.sqrt(Number(value))))-3n;
+	} else {
+		var x1 = (1n << 52n) - 2n;
+	}
 
-    let x0 = -1n;
-    while((x0 !== x1 && x0 !== (x1 - 1n))){
-        x0 = x1;
-        x1 = ((value / x0) + x0) >> 1n;
-    }
-    return x0;
+	let x0 = -1n;
+	while((x0 !== x1 && x0 !== (x1 - 1n))){
+		x0 = x1;
+		x1 = ((value / x0) + x0) >> 1n;
+	}
+	return x0;
 }
 
 module.exports = sqrt;
