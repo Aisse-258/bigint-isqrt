71b31633573cfad05c8f9e973bb6eb896e68cc51
Logan Kearsley <chronosurfer@gmail.com>
2022-09-12 15:30:58 -0600

    Minor speed improvements and a correctness fix.

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-var sqrt = function (value) {
+const i52 = 1n << 52n;
+
+const sqrt = function (value) {
 	if (value < 2n) {
 		return value;
 	}
 
 	if (value < 16n) {
-		return BigInt(Math.floor(Math.sqrt(Number(value))));
+		return BigInt(Math.sqrt(Number(value)|0));
 	}
 	
-	if(value < (1n << 52n)){
-		var x1 = BigInt(Math.floor(Math.sqrt(Number(value))))-3n;
+	let x1;
+	if(value < i52){
+		x1 = BigInt((Math.sqrt(Number(value))|0) - 3);
 	} else {
-		let vlen = value.toString().length;
-		if (vlen%2==0) {
-			var x1 = 10n**(BigInt(vlen/2));
+		const vlen = value.toString().length;
+		if (!(vlen&1)) {
+			x1 = 10n**(BigInt(vlen/2));
 		} else {
-			var x1 = 4n*10n**(BigInt((vlen)/2));
+			x1 = 40n**(BigInt((vlen/2)|0));
 		}
 	}
 
-	let x0 = -1n;
-	while((x0 !== x1 && x0 !== (x1 - 1n))){
+	let x0;
+	do {
 		x0 = x1;
 		x1 = ((value / x0) + x0) >> 1n;
-	}
+	} while ((x0 !== x1 && x0 !== (x1 - 1n)));
 	return x0;
 }
 
