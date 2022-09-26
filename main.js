54ed69fd7a85e3389516a9d8bd3aa9c0ccdd6538
aisse-258 <y-kate@ukr.net>
2022-09-26 16:49:12 +0300

    Some optimization changes by gliese1337

--- main.js
+++ main.js
@@ -4,25 +4,25 @@ var sqrt = function (value) {
 	}
 
 	if (value < 16n) {
-		return BigInt(Math.floor(Math.sqrt(Number(value))));
+		return BigInt(Math.sqrt(Number(value))|0);
 	}
 	
-	if(value < (1n << 52n)){
-		var x1 = BigInt(Math.floor(Math.sqrt(Number(value))))-3n;
+    let x0, x1;
+	if(value < 4503599627370496n){//1n<<52n
+		x1 = BigInt(Math.sqrt(Number(value))|0)-3n;
 	} else {
 		let vlen = value.toString().length;
-		if (vlen%2==0) {
-			var x1 = 10n**(BigInt(vlen/2));
+		if (!(vlen&1)) {
+			x1 = 10n**(BigInt(vlen/2));
 		} else {
-			var x1 = 4n*10n**(BigInt(Math.floor(vlen/2)));
+			x1 = 4n*10n**(BigInt((vlen/2)|0));
 		}
 	}
 
-	let x0 = -1n;
-	while((x0 !== x1 && x0 !== (x1 - 1n))){
+	do {
 		x0 = x1;
 		x1 = ((value / x0) + x0) >> 1n;
-	}
+	}while((x0 !== x1 && x0 !== (x1 - 1n)));
 	return x0;
 }
 
